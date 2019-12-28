9a4b7ec2f53bb266b5566e2d7c4ce315c72df0f4
Hossein Mohammadi <hosseinm.developer@gmail.com>
2019-12-28 09:30:46 +0330

    home page : set StatusBar hidden and background color

---
+++ packages/components/pages/home/styles.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+import {colors} from '@private/core/common';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+});
