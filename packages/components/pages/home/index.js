739783b2c992761a1b4248af4c7f851d0db0db64
Hossein Mohammadi <hosseinm.developer@gmail.com>
2019-12-28 10:15:21 +0330

    add Button to home

--- packages/components/pages/home/index.js
+++ packages/components/pages/home/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
 import styles from './styles';
+import {texts} from '@private/core/common/texts';
+import {Button} from '../../molecules';
 export const Home = () => {
   return (
     <>
       <StatusBar hidden={true} />
-      <SafeAreaView style={styles.container}></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <Button>{texts.START_GAME}</Button>
+      </SafeAreaView>
     </>
   );
 };
